cec9a65483fcbd2b84c1f98ea35ba5204fe222f0
rimshakhan1204 <83567922+rimshakhan1204@users.noreply.github.com>
2023-05-30 13:30:55 +0530

    Add files via upload

---
+++ CurrencyConverter.java
@@ -0,0 +1,108 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CurrencyConverter extends JFrame implements ActionListener {
+    private JLabel amountLabel, fromLabel, toLabel, resultLabel;
+    private JTextField amountField, resultField;
+    private JComboBox<String> fromComboBox, toComboBox;
+    private JButton convertButton;
+
+    public CurrencyConverter() {
+        setTitle("Currency Converter");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 2));
+
+        amountLabel = new JLabel("  Enter Amount :");
+        add(amountLabel);
+        amountField = new JTextField();
+        add(amountField);
+        amountField.setBackground(new Color(176, 196, 222));
+        amountField.setFont(new Font(" Enter Amount", Font.BOLD, 14));
+        amountField .setBounds(170, 53, 250, 26);
+
+        fromLabel = new JLabel("  Convert from :");
+        add(fromLabel);
+        String[] currencies = { "USD $ ", "INR ₹ ", "EUR  € " };
+        fromComboBox = new JComboBox<>(currencies);
+        add(fromComboBox);
+
+        toLabel = new JLabel("  Convert to :");
+        add(toLabel);
+        toComboBox = new JComboBox<>(currencies);
+        add(toComboBox);
+
+        convertButton = new JButton("  Convert  ");
+        convertButton.addActionListener(this);
+        add(convertButton);
+
+        resultLabel = new JLabel("  Result : ");
+        add(resultLabel);
+        resultField = new JTextField();
+        resultField.setEditable(false);
+        add(resultField);
+        resultField.setBackground(new Color(176, 196, 222));
+        resultField.setFont(new Font(" Enter Amount", Font.BOLD, 14));
+
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        double amount = Double.parseDouble(amountField.getText());
+        String fromCurrency = fromComboBox.getSelectedItem().toString();
+        String toCurrency = toComboBox.getSelectedItem().toString();
+        double rate = 0;
+
+        switch (fromCurrency) {
+            case "USD $ ":
+                switch (toCurrency) {
+                    case "USD $ ":
+                        rate = 1;
+                        break;
+                    case "INR ₹ ":
+                        rate = 81.72;
+                        break;
+                    case "EUR € ":
+                        rate = 0.91;
+                        break;
+                }
+                break;
+            case "INR ₹ ":
+                switch (toCurrency) {
+                    case "USD $ ":
+                        rate = 0.012;
+                        break;
+                    case "INR ₹ ":
+                        rate = 1;
+                        break;
+                    case "EUR  € ":
+                        rate = 0.011;
+                        break;
+                }
+                break;
+            case "EUR € ":
+                switch (toCurrency) {
+                    case "USD $ ":
+                        rate = 1.10;
+                        break;
+                    case "INR ₹ ":
+                        rate = 90.08;
+                        break;
+                    case "EUR  € ":
+                        rate = 1;
+                        break;
+                }
+                break;
+        }
+
+        double result = amount * rate;
+        resultField.setText(String.format("%.2f %s", result, toCurrency));
+    }
+
+    public static void main(String[] args) {
+        new CurrencyConverter();
+    }
+}
